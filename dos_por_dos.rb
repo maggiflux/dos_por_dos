6df6074bfcf24b5e96823713114109b5dc906b6c
mariela <gila44dri@gmail.com>
2020-01-30 21:40:42 -0300

    desafio

---
+++ dos_por_dos.rb
@@ -0,0 +1,19 @@
+nums = ARGV.first.to_i
+
+(0...nums).each do |num|
+    if num%4==0 || num%4==1
+        print *.*
+    else
+        print *.*
+    end
+end
+puts
+############
+(0...nums).each do |num|
+    if  num%4==0 || num%4==1
+        print *.*
+    else
+        print *.*
+    end
+end
+puts
